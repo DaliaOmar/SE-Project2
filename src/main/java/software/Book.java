d7b3f84f5e26971def143f8ff8c148a06fb13bc7
unknown <daliamomar123@gmail.com>
2021-04-18 01:02:01 +0300

    done2

--- src/main/java/software/Book.java
+++ src/main/java/software/Book.java
@@ -7,50 +7,12 @@ public class Book {
 	private String signature;
 	private String isbn;
 
-	public String getTitle() {
-		return title;
-	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getSignature() {
-		return signature;
-	}
-
-	public void setSignature(String signature) {
-		this.signature = signature;
-	}
-
-	public String getISBN() {
-		return isbn;
-	}
-
-	public void setISBN(String isbn) {
-		isbn = isbn;
-	}
-
-	public Book() {
-		this.isbn = "";
-		this.title = "";
-		this.author = "";
-		this.signature = "";
-	}
-
-	public Book(String title, String author, String signature, String isbn) {
+	public Book(String title, String author, String signature, String isbnn) {
 		this.title = title;
 		this.author = author;
 		this.signature = signature;
-		this.isbn = isbn;
+		this.isbn = isbnn;
 
 	}
 
